9aa27d45b791eea7cabc887e6405c98260bfad6b
Paulo Bertanha <you@example.com>
2022-01-06 15:00:50 -0300

    Add project files.

---
+++ pooCalculadora/Calculadora.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculadora
+{
+    public class Calculo
+    {
+        public decimal valorA { get; set; }
+        public decimal valorB { get; set; }
+
+        public char op { get; set; }
+
+        public Calculo(decimal a, decimal b, char operador)
+        {
+            this.valorA = a;
+            this.valorB = b;
+            this.op = operador;
+        }
+
+        public decimal Calcular(Calculo a)
+        {
+
+        }
+    }
+}
